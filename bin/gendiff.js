f23c3dd22ed6e0b8329e3a24d74d5fd3cbb9dc89
Dmitry <dm.mudryak@gmail.com>
2020-07-28 09:35:44 +0300

    frontend-project-lvl2 step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ const { program } = commander;
 program
   .version('0.0.1', '-v, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format');
 
